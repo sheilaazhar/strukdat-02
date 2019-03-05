c1eecda9f753800d6128ff66aa669e3dd142b33c
sheilaazhar <47959466+sheilaazhar@users.noreply.github.com>
2019-03-05 09:29:37 +0700

    Update pretest.cpp

--- pretest.cpp
+++ pretest.cpp
@@ -9,33 +9,34 @@ Nama Program: Move Zero to The End Of Array
 
 using namespace std;
 
-void moveZeroToEnd(int arr[], int n){
-	int count = 0;
-    for (int i = 0; i < n; i++) 
-        if (arr[i] != 0) 
-            arr[count++] = arr[i];
-    while (count < n) 
-        arr[count++] = 0;
+void input(int arr[],int& n){
+    cout << "Masukkan Panjang Data : ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+}
+void moveZerosToEnd(int arr[], int n)
+{
+    int count=0;
+    for (int i=0; i<n;i++)
+        if (arr[i] != 0)
+            swap(arr[count++], arr[i]);
+}
+void output(int arr[],int n){
+    cout << "Hasil  : ";
+    for (int i = 0;i<n;i++)
+    cout << arr[i] << " ";
 }
 
-void input(int (&arr)[100], int& n){
-	cout<<"Masukkan panjang data : ";cin>>n;
-	cout<<"Masukkan Data : ";
-	for(int i=0; i<n; i++){
-		;cin>>i;
-	}
-}
 
-void output(int arr[], int n){
-    cout << "Hasil : "; 
-    for (int i = 0; i < n; i++) 
-        cout << arr[i] << " "; 
-}
+int main()
+{
+    int arr[100];
+    int n;
+    input(arr,n);
+    moveZerosToEnd(arr,n);
+    output(arr,n);
 
-int main(){
-	int arr[100];
-	int n;
-	input(arr, n);
-	moveZeroToEnd(arr,n);
-	output(arr,n);
+    return 0;
 }
